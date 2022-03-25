2b44a26031ce839060570a02dd842e2f0d5a8c80
nai <danai.emelia@gmail.com>
2022-03-25 11:00:15 -0400

    first js test

---
+++ script.js
@@ -0,0 +1,8 @@
+function buttonClick() {
+    var x = document.getElementById("priceChangeSummary");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
