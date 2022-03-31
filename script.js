d5b711d961f1bb86f921c8a6ff892fe1b46f62c4
nai <danai.emelia@gmail.com>
2022-03-31 00:00:35 -0400

    hide bottom bar until user confirms correct info

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ function buttonClick() {
   } else {
     x.style.display = "none";
   }
+  var x = document.getElementById("bottomBar");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
 }
